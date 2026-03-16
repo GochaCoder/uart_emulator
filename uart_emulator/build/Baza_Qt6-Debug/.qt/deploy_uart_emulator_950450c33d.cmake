605ac9bb6caa3e71516479ca3f61776d662eb483
GochaCoder <65740670+GochaCoder@users.noreply.github.com>
2026-03-17 00:43:16 +0300

    .

--- uart_emulator/build/Baza_Qt6-Debug/.qt/deploy_uart_emulator_950450c33d.cmake
+++
@@ -1,8 +0,0 @@
-include("D:/CCode/QtProjects/uart_emulator/uart_emulator/build/Baza_Qt6-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/uart_emulator-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtserialport")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "D:/CCode/QtProjects/uart_emulator/uart_emulator/build/Baza_Qt6-Debug/uart_emulator.exe"
-    GENERATE_QT_CONF
-)
